a21fb342f5537bff6f7c8029643d8f7947f022f9
Nikolai Sitnikov <nikolai.sitnikov@gmail.com>
2015-12-17 13:11:11 +0300

    Finally manage to map to data.frame

--- R/rxes.R
+++ R/rxes.R
@@ -1,16 +1,27 @@
-initXESTools <- function() {
+xes.init <- function() {
     xesTools <- .jnew("ru.ramax.processmining.XEStools")
     return (xesTools)
 }
 
-xesParseLog <- function(xesTools, filename) {
+xes.parseLog <- function(xesTools, filename) {
     # returned object success or failure
     result <- .jcall(xesTools, "Z", "parseLog", filename  )
 
     return (result)
 }
 
-xesTraceCount <- function(xesTools) {
+xes.traceCount <- function(xesTools) {
     result <- .jcall(xesTools, "I", "getXLogSize")
     return (result)
+}
+
+xes.getTraceDurations <- function(xesTools) {
+    res <- .jcall(xesTools, "Ljava/util/Map;", "getTraceDurations")
+    kset <- .jcall(res, "Ljava/util/Set;", "keySet")
+    kSetA <- .jcall(kset, "[Ljava/lang/Object;", "toArray")
+    traces <- sapply(kSetA, function(item) { .jstrVal(item)})
+    values <- .jcall(res, "Ljava/util/Collection;", "values")
+    valuesA <- .jcall(values, "[Ljava/lang/Object;", "toArray")
+    durations <- sapply(valuesA, function(item) {  .jcall(item, "J", "longValue") })
+    df <- data.frame(trace = traces, duration = durations)
 }
