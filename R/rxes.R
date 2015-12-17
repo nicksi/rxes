9a4d4384c75425c26bb710522757737ac338539f
Nikolai Sitnikov <nikolai.sitnikov@gmail.com>
2015-12-17 16:02:30 +0300

    documentation attempts

--- R/rxes.R
+++ R/rxes.R
@@ -1,8 +1,23 @@
+#' Initialize XES Log processing class
+#'
+#' \code{xes.init} returns pointer to the javaClass with empty log
+#'
+#' You should pass this reference to the other funcions, so we can use cache and avoiud log reload on any
+#' operation
+#'
+#' @return reference to XESTools object
 xes.init <- function() {
     xesTools <- .jnew("ru.ramax.processmining.XEStools")
     return (xesTools)
 }
 
+#' Loads and parses XES log in GZIP xml format
+#'
+#' loads and saves in the class object log specified by the filename
+#'
+#' @param xesTools reference to XEStools class object created via \code{\link{xes.init}}
+#' @param filename string pointing to the event log file
+#' @return boolean success flag
 xes.parseLog <- function(xesTools, filename) {
     # returned object success or failure
     result <- .jcall(xesTools, "Z", "parseLog", filename  )
@@ -10,11 +25,19 @@ xes.parseLog <- function(xesTools, filename) {
     return (result)
 }
 
+#' Return count of traces in the event log
+#'
+#' @param xesTools reference to XEStools class object created via \code{\link{xes.init}}
+#' @return success flag
 xes.traceCount <- function(xesTools) {
     result <- .jcall(xesTools, "I", "getXLogSize")
     return (result)
 }
 
+#' Return data.frame with the trace name (concept:name) and trace duration expressed in seconds
+#'
+#' @param xesTools reference to XEStools class object created via \code{\link{xes.init}}
+#' @return data.frame with trace durations
 xes.getTraceDurations <- function(xesTools) {
     res <- .jcall(xesTools, "Ljava/util/Map;", "getTraceDurations")
     kset <- .jcall(res, "Ljava/util/Set;", "keySet")
